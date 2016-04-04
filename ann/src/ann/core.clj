fc174492bc4c058c4cd97c441596d02c180206e6
Owain Lewis <owain@owainlewis.com>
2016-04-04 19:40:01 +0100

    update py code

--- ann/src/ann/core.clj
+++ ann/src/ann/core.clj
@@ -1,6 +1,21 @@
 (ns ann.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- dot-product
+  [v1 v2]
+  (reduce + (map * v1 v2)))
+
+(defn random-weights
+  "Generate n random positive OR negative weights"
+  [n]
+  (take n (repeatedly #(- 0.5 (rand)))))
+
+(defn perceptron []
+  {:weights []
+   :learning-rate 0.1})
+
+(defn threshold? [n]
+  (if (> n 0) 1 -1))
+
+(defn update-weights [perceptron])
+
+(defn feed-forward [perceptron])
--- perceptron.py
+++ perceptron.py
@@ -1,32 +1,37 @@
-class Perceptron():
-    def __init__(self):
-        pass
+from random import random
 
 def dot_product(inputs, weights):
-    return sum(i*w for i,w in zip(inputs, weights))
+    return sum(float(x * y) for x, y in zip(inputs, weights))
 
-threshold = 0.5 # activation threshold
-learning_rate = 0.1
-weights = [0, 0, 0]
-training_set = [([1, 0, 0], 1), ([1, 0, 1], 1), ([1, 1, 0], 1), ([1, 1, 1], 0)]
-error_count = 0
+def sign(n):
+    return 1 if n > 0 else -1
 
-def iteration():
-    for input_vector, desired_output in training_set:
-        print("Input %s" % input_vector)
-        print("Weights %s" % weights)
-        dp = dot_product(input_vector, weights)
-        result = dp > threshold
-        print(result)
-        error = desired_output - result
+class Perceptron:
+    """ Simple perceptron for linear classification """
+    def __init__(self, n, learning_constant = 0.1):
+        self.n = n
+        self.weights = []
+        # With a high learning constant the algo will converge more quickly but may
+        # be less accurate than it would be using a smaller learning constant
+        self.constant = learning_constant
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        """ Generate random weights (+ AND -) for the initial state """
+        for i in range(self.n):
+            self.weights.append((random() - 0.5))
+
+    def feed_forward(self, inputs):
+        """ Make a prediction about classification """
+        # Take the dot product of the input and weights
+        dot = dot_product(inputs, self.weights)
+        return sign(dot)
+
+    def calculate_new_weight(self, error ,value):
+        return self.contant * error * value
+
+    def train(self, inputs, goal):
+        guess = self.feed_forward(inputs)
+        print(guess)
+        error = goal - guess
         print(error)
-        if error != 0:
-            error_count += 1
-            for index, value in enumerate(input_vector):
-                weights[index] += learning_rate * error * value
-
-def learn():
-    while True:
-        iteration()
-        if error_count == 0:
-            break
